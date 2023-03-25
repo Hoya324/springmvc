830b00d8efeb75681fc92244fd6115290a34b59f
Hoya324 <gangho324@naver.com>
2023-03-25 14:55:59 +0900

    2023.03.25

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,31 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RestController
+public class LogTestController {
+    //@Slf4j로 생략 가능
+//    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    private String logTest() {
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+
+//        log 레벨 순
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info(" info log={}", name);
+        log.warn(" warn log={}", name);
+        log.error("error log={}", name);
+
+        return "ok";
+    }
+}
